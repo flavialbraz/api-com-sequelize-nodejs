27986ebe12349e63e62b9bcbf847d311adb44ab5
Flávia <flaviabrazx@gmail.com>
2022-06-05 22:12:15 -0300

    :tada: Api finalizada

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,15 @@
 // Ponto de entrada de todas as rotas 
 const bodyParser = require('body-parser')
 const pessoas = require('./pessoasRouter')
-
+const niveis = require('./niveisRoute')
+const turmas = require('./turmasRoute')
 
 module.exports = app => {
-    app.use(bodyParser.json())
-    app.use(pessoas)
-}
+ app.use(
+   bodyParser.json(),
+   bodyParser.urlencoded({ extended: false }),
+   pessoas,
+   niveis,
+   turmas
+   )
+ }
--- api/routes/pessoasRouter.js
+++ api/routes/pessoasRouter.js
@@ -1,15 +1,16 @@
 const { Router } = require('express')
 const PessoaController = require('../controllers/PessoaController')
 
-// esse router é do express
-const router = Router()
+const router = Router() // do Express
 
-
-router.get('/pessoas', PessoaController.pegaTodasAsPessoas )
-router.get('/pessoas/:id', PessoaController.pegaUmaPessoa )
-router.post('/pessoas', PessoaController.criaPessoa )
-router.put('/pessoas/:id', PessoaController.atualizaPessoa ) // Atualiza registro
-router.delete('/pessoas/:id', PessoaController.apagaPessoa ) // Deleta registro
-
+router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+router.get('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.pegaUmaMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.atualizaMatricula)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId',  PessoaController.apagaMatricula)
 
 module.exports = router
